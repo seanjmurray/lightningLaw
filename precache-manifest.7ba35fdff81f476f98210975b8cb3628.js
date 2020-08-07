3cc4aa8bb464000a45fec4ecc096744515b50e18
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-07 20:52:43 +0000

    Deploy seanjmurray/lightningLaw to github.com/seanjmurray/lightningLaw.git:gh-pages

---
+++ precache-manifest.7ba35fdff81f476f98210975b8cb3628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ffb7c29833ae9611d008e693c64b0a",
+    "url": "/lightningLaw/index.html"
+  },
+  {
+    "revision": "8a631974c4450b32dc46",
+    "url": "/lightningLaw/static/css/main.39e90ccb.chunk.css"
+  },
+  {
+    "revision": "b3919388f4cc21293653",
+    "url": "/lightningLaw/static/js/2.01efb349.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lightningLaw/static/js/2.01efb349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a631974c4450b32dc46",
+    "url": "/lightningLaw/static/js/main.3328794e.chunk.js"
+  },
+  {
+    "revision": "fee7504ed8cd177b4906",
+    "url": "/lightningLaw/static/js/runtime-main.a0a42e04.js"
+  }
+]);
